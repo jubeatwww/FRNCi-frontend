c59b517fe5bf5d8d53fa6d598d6ac52526acffcf
JubeatWwW <l6104400@gmail.com>
2017-10-11 17:39:55 +0800

    add on-boards

---
+++ src/components/OnBoard.vue
@@ -0,0 +1,67 @@
+<<template>
+    <md-boards id="on-board"
+        :md-auto="true" 
+        :md-infinite="true" 
+        :md-duration="5000" 
+        :md-swipeable="true" 
+        :md-controls="true">
+        
+        <md-board class="boards" v-for="slide in slides">
+            <div>
+                <img>
+                <p>{{ slide }}</p>
+            </div>
+        </md-board>
+    </md-boards>
+</template>
+
+<<script>
+export default {
+    data() {
+        return {
+            slides: ['a', 'b', 'c'],
+        };
+    },
+};
+</script>
+
+<style>
+#on-board {
+    width: 100%;
+    height: 80vh !important;
+}
+
+#on-board .boards{
+    padding: 0;
+}
+
+#on-board img{
+    width: 100%;
+    height: 100vh;
+}
+
+#on-board nav{
+    position: absolute;
+    top: 0;
+    height: 100%;
+}
+
+#on-board nav button{
+    display: none;
+}
+
+#on-board nav button:first-child{
+    display: initial;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+}
+
+#on-board nav button:nth-last-child(2){
+    display: initial;
+    position: absolute;
+    top: 50%;
+    right: 0;
+    transform: translateY(-50%);
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,19 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+// import HelloWorld from '@/components/HelloWorld';
+import OnBoard from '@/components/OnBoard';
+import VueMaterial from 'vue-material';
+import 'vue-material/dist/vue-material.css';
 
+Vue.use(VueMaterial);
 Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: HelloWorld,
-    },
-  ],
+    routes: [
+        {
+            path: '/',
+            name: 'Hello',
+            component: OnBoard,
+        },
+    ],
 });
