e6849a88374e7e35b8841e711c6b3f27668a5895
JubeatWwW <l6104400@gmail.com>
2017-12-28 15:38:30 +0800

    basic profile navbar

--- src/components/Profile/Nav.vue
+++ src/components/Profile/Nav.vue
@@ -1,13 +1,55 @@
 <template>
-    <nav>
+    <nav :style="bgStyle">
+        <figure><img :src="avatar"/></figure>
+        <h2>{{name}}</h2>
+        <button></button>
     </nav>
 </template>
 
 <script>
 export default {
+    props: {
+        bg: {
+            type: String,
+            default: '',
+        },
+        avatar: {
+            type: String,
+            default: 'static/img/logo_frnci.png',
+        },
+        name: {
+            type: String,
+            default: 'FRNCi',
+        },
+    },
+    computed: {
+        bgStyle() {
+            return {
+                background: `${this.bg} no-repeat center center`,
+                height: '200px',
+            };
+        },
+    },
 };
 </script>
 
 <style lang="scss" scoped>
 
+figure {
+    position: relative;
+    float: left;
+    border: 3px solid gray;
+    border-radius: 150%;
+    overflow: hidden;
+    margin: 0;
+    height: 150px;
+    width: 150px;
+
+    img {  
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -24,7 +24,6 @@ export default new Router({
     routes: [
         {
             path: '/',
-            name: 'Main',
             component: Main,
             children: [
                 {
