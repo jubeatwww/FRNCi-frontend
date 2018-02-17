b3780cd6838082517f78b347755f749f2adb83ce
JubeatWwW <l6104400@gmail.com>
2018-02-17 13:59:34 +0800

    fix checkbox group default

--- src/components/CustomComponents/CheckBoxGroup.vue
+++ src/components/CustomComponents/CheckBoxGroup.vue
@@ -2,7 +2,7 @@
     <div class="checkbox-group"
         style="display: flex">
         <div v-for="(optCol, colIdx) in _optionsColumns" 
-            :key="`${name}-checkbox-group-col-${optCol}`"
+            :key="`${name}-checkbox-group-col-${colIdx}`"
             :style="columnStyle">
             <md-checkbox
                 v-for="(opt, idx) in optCol"
@@ -79,6 +79,14 @@ export default {
             this.disabledList[idx] = !this.disabledList[idx];
             this.$emit('update:value', this.selected);
         },
+    },
+    created() {
+        this.default.map((val) => {
+            const idx = this.options.findIndex(opt => opt.value === val);
+            console.log(idx);
+            this.disabledList[idx] = false;
+            return false;
+        });
     },
 };
 </script>
