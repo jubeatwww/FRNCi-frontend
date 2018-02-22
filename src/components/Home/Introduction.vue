be6c59d2534043e1ed47ffdbc90a377b80558844
JubeatWwW <l6104400@gmail.com>
2018-02-23 03:28:19 +0800

    static content

---
+++ src/components/Home/Introduction.vue
@@ -0,0 +1,35 @@
+<template>
+    <section>
+        <md-layout md-align="center">
+            <md-layout md-column="true" md-align="center">
+            <h3>Glocal Click 是什麼？</h3>
+                <p>
+                    Glocal Click 是 FRNCi 將推出的全新平台，主打<b>「與跨文化朋友自在說外語」的全方位體驗</b><br>
+                    有別於一般語言交換 / 學習平台，Glocal Click 透過線上媒合服務和線下交流活動<br>
+                    讓你在練習外語的同時，結交來自世界各地的朋友，增進國際思維與在地視野
+                </p>
+                <p>
+                    Glocal 取自於 Global 和 Local，也就是全球在地化；Click 則有一拍即合、一次到位的意涵 <br>
+                    Glocal Click 讓你一鍵連結世界，輕鬆找到能見面練外語的國際朋友
+                </p>
+            </md-layout>
+        </md-layout>
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+        };
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+section {
+    padding: 48px 0;
+    background-color: #f8b62c;
+    color: white;
+}
+</style>
